cae230a37e57d624ee29448bd45360e31c9a4ca3
Julio Méndez <60590908+juliotorres82@users.noreply.github.com>
2020-12-08 13:02:56 -0600

    Add files via upload

---
+++ 8_diciembre/main.py
@@ -0,0 +1,14 @@
+from backtraking import LaberintoADT
+from array2d import Array2D
+import time
+pasillos_inicial = ((2,1),(2,2),(2,3),(2,4),(3,2),(4,2))
+lab = LaberintoADT (6, 6, pasillos_inicial, (5,2), (2,5))
+lab.buscar_entrada()
+lab.to_string()
+lab.imprimir_camino()
+
+while not lab.es_salida (lab.get_pos_actual()[0], lab.get_pos_actual()[1]):
+    print("probar")
+    lab.resolver_laberinto()
+    lab.imprimir_camino
+    time.sleep(1.0)
